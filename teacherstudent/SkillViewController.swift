ae843436b7f633b5ddac94ce3146136b3cec340b
ch4newsteam <ryan-krutwig@uiowa.edu>
2017-11-08 14:51:05 -0600

    added to teacher the zip and near me code for posting a new session

--- teacherstudent/SkillViewController.swift
+++ teacherstudent/SkillViewController.swift
@@ -9,11 +9,21 @@
 
 import Foundation
 import UIKit
+import CoreLocation
+import AddressBookUI
+import CoreLocation
 
-class SkillViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class SkillViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,CLLocationManagerDelegate {
 
     @IBOutlet weak var selectedSkillLabel: UILabel!
     @IBOutlet weak var skillPickerView: UIPickerView!
+    let locationmanger = CLLocationManager.init()
+    var controlswitch = true
+    var maxlatrange = 0.0
+    var maxlongrange = 0.0
+    var zipcode = 0
+    var distance = 0.0
+    var subject = ""
     
     var selectedSkill: String?
     
@@ -24,6 +34,7 @@ class SkillViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         skills = ["Basketball", "Cooking", "Coding", "Mathematics", "Microsoft Office"]
         selectedSkill = skills[0]
         selectedSkillLabel.text = selectedSkill
+          locationmanger.startUpdatingLocation()
     }
     
     override func didReceiveMemoryWarning() {
@@ -47,4 +58,62 @@ class SkillViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         selectedSkillLabel.text = selectedSkill
     }
     
+    @IBAction func zipcodedistance(_ sender: UITextField) {
+        
+        distance = Double(sender.text!)!
+        
+    }
+    func forwardGeocoding(address: String){
+        CLGeocoder().geocodeAddressString(address, completionHandler: { (placemarks, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if (placemarks?.count)! > 0 {
+                let placemark = placemarks?[0]
+                let location = placemark?.location
+                let coordinate = location?.coordinate
+                print("\nlat: \(coordinate!.latitude), long: \(coordinate!.longitude)")
+                self.maxlongrange = (coordinate?.longitude)! as Double
+                self.maxlatrange = (coordinate?.latitude)! as Double
+                if (placemark?.areasOfInterest?.count)! > 0 {
+                    let areaOfInterest = placemark!.areasOfInterest![0]
+                    print(areaOfInterest)
+                } else {
+                    print("No area of interest found.")
+                }
+            }
+        })
+    }
+    @IBAction func zipcode(_ sender: UITextField) {
+        
+        forwardGeocoding(address: sender.text!)
+        
+        
+    }
+  
+    @IBAction func dateandtime(_ sender: UIDatePicker) {
+    }
+    @IBAction func subjectlinebox(_ sender: UITextField) {
+        subject = sender.text!
+    }
+    @IBAction func locationswitch(_ sender: UISwitch) {
+        if(controlswitch)
+        {
+            locationmanger.delegate = self
+            locationmanger.desiredAccuracy = 10
+            
+            print(locationmanger.location!)
+            controlswitch = false
+        }
+        else{
+            controlswitch = true
+        }
+    }
+    
+  
+    // testing
 }
+
+
+
